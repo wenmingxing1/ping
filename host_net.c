64facb9b695e1a1270927b31dcd94e79e0420354
ligang <1793701192@qq.com>
2018-04-06 17:00:49 +0800

    change

--- host_net.c
+++ host_net.c
@@ -10,7 +10,7 @@ int main() {
     nets = htons(hosts);
     netl = htonl(hostl);
 
-    printf("Host ordered short: %#x \n", hosts);
+    printf("Host ordered short: %#x \n", hosts);  //主机字节为小端序
     printf("Network ordered short: %#x \n", nets);
     printf("Host ordered long: %#lx \n", hostl);
     printf("Network ordered long: %#lx \n", netl);
