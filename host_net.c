042fa3a6d36aee049565f97fccb4a515b4450e5f
ligang <1793701192@qq.com>
2018-04-06 16:11:46 +0800

    大端模式与小端模式函数

---
+++ host_net.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<arpa/inet.h>
+
+int main() {
+    unsigned short hosts = 0x1234;
+    unsigned short nets;
+    unsigned long hostl = 0x12345678;
+    unsigned long netl;
+
+    nets = htons(hosts);
+    netl = htonl(hostl);
+
+    printf("Host ordered short: %#x \n", hosts);
+    printf("Network ordered short: %#x \n", nets);
+    printf("Host ordered long: %#lx \n", hostl);
+    printf("Network ordered long: %#lx \n", netl);
+
+    return 0;
+}
